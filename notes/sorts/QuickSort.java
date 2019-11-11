373402a64f97d78cb328d1356fcc1fff54ced516
Robert Schaedler III <robertschaedler3@gmail.com>
2019-11-11 14:42:22 -0500

    add quicksort partition

---
+++ notes/sorts/QuickSort.java
@@ -0,0 +1,48 @@
+package notes.sorts;
+
+import java.util.*;
+
+public class QuickSort {
+
+
+    public static void sort(int[] arr) {
+
+    }
+
+    private static void sort(int[] arr, int left, int right) {
+
+    }
+
+    private static void partition(int[] arr, int left, int right) {
+        if (arr.length <= 1)
+            return;
+
+        int pivot = left;
+        do {
+            while (left < arr.length && arr[left] <= arr[pivot])
+                left++;
+            while(arr[right] > arr[pivot]) 
+                right--;
+
+            if (left < right)
+                swap(arr, left, right);
+            
+        } while (left < right);
+
+        swap(arr, pivot, right);
+    }
+    
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 44, 75, 23, 43, 55, 12, 64, 77, 33 };
+        System.out.println((Arrays.toString(arr)));
+        partition(arr, 0, arr.length - 1);
+        System.out.println((Arrays.toString(arr)));
+    }
+    
+}
