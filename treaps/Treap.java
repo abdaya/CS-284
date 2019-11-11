7287be7886db2c50ae33d312210ed9391ca2b7be
Robert Schaedler III <robertschaedler3@gmail.com>
2019-11-11 01:19:44 -0500

    implemented private Node class

--- treaps/Treap.java
+++ treaps/Treap.java
@@ -1,5 +1,30 @@
 package treaps;
 
-public class Treap {
+public class Treap<E extends Comparable<E>> {
+
+    private static class Node<F> {
+
+        private int priority;
+        private F key;
+        private Node<F> left;
+        private Node<F> right;
+
+        public Node(F key, int priority) {
+            this.key = key;
+            this.priority = priority;
+            this.left = null;
+            this.right = null;
+        }
+
+        public Node(F key, int priority, Node<F> left, Node<F> right) {
+            this.key = key;
+            this.priority = priority;
+            this.left = left;
+            this.right = right;
+        }
+
+    }
+
+
     
 }
