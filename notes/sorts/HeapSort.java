a55c385aced503fcaf78bad6590280e98cd3f954
Robert Schaedler III <robertschaedler3@gmail.com>
2019-11-15 15:16:19 -0500

    improved build heap

--- notes/sorts/HeapSort.java
+++ notes/sorts/HeapSort.java
@@ -1,22 +1,45 @@
 package notes.sorts;
 
+import java.util.Arrays;
+
 public class HeapSort {
 
     public static void sort(int[] arr) {
 
-        // build max heap
-        for (int i = 1; i < arr.length; i++) {
-            int child = i;
-            int parent = (child - 1) / 2;
-            while (child >= 0 && arr[child] > arr[parent]) {
-                swap(arr, child, parent);
-                child = parent;
-                parent = (child - 1) / 2;
+        // build max heap O(N)
+        for (int i = (arr.length / 2) - 1; i >= 0; i--) {
+            // percolate down
+            int parent = i;
+            int left = (2 * parent) + 1;
+            int right = (2 * parent) + 2;
+            while (right <= arr.length) {
+                int maxChild = left;
+                if (right < arr.length && arr[right] > arr[left])
+                    maxChild = right;
+
+                if (arr[parent] > arr[maxChild])
+                    break;
+
+                swap(arr, parent, maxChild);
+                parent = maxChild;
+                left = (2 * parent) + 1;
+                right = (2 * parent) + 2;
             }
         }
 
-        // shrink heap
-        for(int end = arr.length - 1; end > 0; end--) {
+        // build max heap O(N log N)
+        // for (int i = 1; i < arr.length; i++) {
+        //     int child = i;
+        //     int parent = (child - 1) / 2;
+        //     while (child >= 0 && arr[child] > arr[parent]) {
+        //         swap(arr, child, parent);
+        //         child = parent;
+        //         parent = (child - 1) / 2;
+        //     }
+        // }
+
+        // shrink heap O(N log N)
+        for (int end = arr.length - 1; end > 0; end--) {
             swap(arr, 0, end);
 
             // re-heap
@@ -25,14 +48,13 @@ public class HeapSort {
             int right = (2 * parent) + 2;
 
             while (right <= end) {
-
                 int maxChild = left;
-                if(right < end && arr[right] > arr[left]) 
+                if (right < end && arr[right] > arr[left])
                     maxChild = right;
 
                 if (arr[parent] > arr[maxChild])
                     break;
-                
+
                 swap(arr, parent, maxChild);
                 parent = maxChild;
                 left = (2 * parent) + 1;
@@ -49,15 +71,10 @@ public class HeapSort {
     
     public static void main(String[] args) {
         int[] arr = { 74, 66, 89, 6, 39, 29, 76, 32, 18, 28, 37, 26, 20 };
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + ", ");
-        }
-        System.out.println();
+        // int[] arr = { 10,6,9,5,3,8,7,4,2,1 };
+        System.out.println(Arrays.toString(arr));
         HeapSort.sort(arr);
-
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + ", ");
-        }
+        System.out.println(Arrays.toString(arr));
     }
     
 }
