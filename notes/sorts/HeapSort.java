c7de3745e97c8c02ec509ad996818cebe8e8fb31
Robert Schaedler III <robertschaedler3@gmail.com>
2019-12-06 00:10:17 -0500

    remove old code

--- notes/sorts/HeapSort.java
+++ notes/sorts/HeapSort.java
@@ -37,60 +37,13 @@ public class HeapSort {
     public static void sort(int[] arr) {
 
         // build max heap O(N)
-        for (int i = (arr.length / 2) - 1; i >= 0; i--) {
-            // percolate down
+        for (int i = (arr.length / 2) - 1; i >= 0; i--) 
             percolateDown(arr, i, arr.length);
-            // int parent = i;
-            // int left = (2 * parent) + 1;
-            // int right = (2 * parent) + 2;
-            // while (right <= arr.length) {
-            //     int maxChild = left;
-            //     if (right < arr.length && arr[right] > arr[left])
-            //         maxChild = right;
-
-            //     if (arr[parent] > arr[maxChild])
-            //         break;
-
-            //     swap(arr, parent, maxChild);
-            //     parent = maxChild;
-            //     left = (2 * parent) + 1;
-            //     right = (2 * parent) + 2;
-            // }
-        }
-
-        // build max heap O(N log N)
-        // for (int i = 1; i < arr.length; i++) {
-        //     int child = i;
-        //     int parent = (child - 1) / 2;
-        //     while (child >= 0 && arr[child] > arr[parent]) {
-        //         swap(arr, child, parent);
-        //         child = parent;
-        //         parent = (child - 1) / 2;
-        //     }
-        // }
 
         // shrink heap O(N log N)
         for (int end = arr.length - 1; end > 0; end--) {
             swap(arr, 0, end);
             percolateDown(arr, 0, end);
-            // re-heap
-            // int parent = 0;
-            // int left = (2 * parent) + 1;
-            // int right = (2 * parent) + 2;
-
-            // while (right <= end) {
-            //     int maxChild = left;
-            //     if (right < end && arr[right] > arr[left])
-            //         maxChild = right;
-
-            //     if (arr[parent] > arr[maxChild])
-            //         break;
-
-            //     swap(arr, parent, maxChild);
-            //     parent = maxChild;
-            //     left = (2 * parent) + 1;
-            //     right = (2 * parent) + 2;
-            // }
         }
     }
     
